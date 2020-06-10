a9094e4e0acd62f594422055068673ba1456295e
StefanMitrikeski <mi34ence94@gmail.com>
2020-06-10 03:05:13 +0200

    breaking into smaller components and styling our components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './homepage.scss';
+import Directory from '../../components/directory/directory.jsx';
+
+const HomePage = () => (
+    <div className='hompage'>
+        <Directory />
+    </div>
+);
+export default HomePage;

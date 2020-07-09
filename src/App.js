ad97711afe3f12950190b1d673c0981821bb724e
StefanMitrikeski <mi34ence94@gmail.com>
2020-07-09 03:39:38 +0200

    added redux and implemented userReducer  and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,59 +1,63 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import './App.css';
-import  HomePage  from './pages/homepage/homepage'
-import ShopPage from './pages/shop/shop';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import Header from './components/header/header';
-import { auth,createUserProfileDocument } from './firebase/firebase.utils';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+import "./App.css";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import Header from "./components/header/header";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
+  // constructor() {
+  //   super();
 
-  constructor(){
-    super();
+  //   this.state = {
+  //     currentUser: null,
+  //   };
+  // }
 
-    this.state = {
-      currentUser: null
-    }
-  }
+  unsubscribeFromAuth = null;
 
-  unsubscribeFromAuth = null
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
-  componentDidMount (){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
-
-      if(userAuth){
-        
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot(snapShot =>{
-          this.setState({
-            currentUser:{
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          })
-        })
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
+        });
       }
-      this.setState({currentUser: userAuth})
+      setCurrentUser(userAuth);
     });
   }
 
-  componentWillUnmount (){
-      this.unsubscribeFromAuth();
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <Header currentUser={ this.state.currentUser }/>
+        <Header />
         <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUp}/>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapToDispatch = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapToDispatch)(App);
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ShopData from './shop.data.js'
-import CollectionPreview from '../../components/collection-preview/collections-preview'
+import React from "react";
+import ShopData from "./shop.data.js";
+import CollectionPreview from "../../components/collection-preview/collections-preview";
 
 class ShopPage extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            collections: ShopData
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      collections: ShopData,
+    };
+  }
 
-    render(){
+  render() {
     const { collections } = this.state;
-    return (<div className='shop-page'>
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
-    </div>)
-    }
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 export default ShopPage;
